9343f9aff8bcd5669ba2e1057ec9e4c6807eaf28
Dr.Moo <obries6@rpi.edu>
2017-05-26 15:21:17 -0400

    Initial Commit: incomplete nick function

---
+++ chatserver.py
@@ -0,0 +1,137 @@
+# chat_server.py
+
+import sys
+import socket
+import select
+
+HOST = ''
+SOCKET_LIST = []
+RECV_BUFFER = 4096
+PORT = 9009
+
+##Server Info
+stored_names = []
+
+def command(server_socket, sock, data):
+    #print "printing sock name"
+    #socket_name = socket.getsockname()
+    #print socket_name
+
+    print "before: "
+    print stored_names
+
+    datal = data.split(' ')
+    if datal[0] == '/changename':
+        send_string = "Server: Changing name to " + datal[1]
+        sock.send(send_string)
+        nickname_to_change = (sock.getpeername())[1]
+        i = 0
+        while i < len(stored_names):
+            if stored_names[i][0] == nickname_to_change:
+                print "datal[1]: ", datal[1]
+                datal[1].replace("\n",'')
+                print "datal[1]: ", datal[1]
+                stored_names[i][1] = datal[1]
+                #print stored_names
+                #print datal
+
+            i += 1
+
+    print "after:"
+    print stored_names
+        #print sock.gethostname()
+
+
+def chat_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(10)
+
+    # add server socket object to the list of readable connections
+    SOCKET_LIST.append(server_socket)
+
+    print "Chat server started on port " + str(PORT)
+
+    while 1:
+
+        # get the list sockets which are ready to be read through select
+        # 4th arg, time_out  = 0 : poll and never block
+        ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST, [], [], 0)
+
+        for sock in ready_to_read:
+            # a new connection request recieved
+            if sock == server_socket:
+                sockfd, addr = server_socket.accept()
+                SOCKET_LIST.append(sockfd)
+                print "Client (%s, %s) connected" % addr
+                stored_names.append([addr[1], ''])
+                print stored_names
+
+                broadcast(server_socket, sockfd, "[%s:%s] entered our chatting room\n" % addr)
+
+            # a message from a client, not a new connection
+            else:
+                # process data received from client,
+                try:
+                    # receiving data from the socket.
+                    data = sock.recv(RECV_BUFFER)
+                    if data:
+                        # there is something in the socket
+                        print data, '\n'
+                        if data[0][0] == "/":
+                            print "ello"
+                            command(server_socket, sock, data)
+                            print "guvnah"
+
+                        checknick = (sock.getpeername())[1]
+                        print "checknick: ", checknick
+
+                        nick = False
+                        i = 0
+                        while i < len(stored_names):
+                            print "i: ", i
+                            if checknick == stored_names[i][0] and stored_names[i][1] != '':
+                                print "hit: "
+                                broadcast(server_socket, sock, "\r" + '[' + stored_names[i][1][0:-1] + '] ' + data)
+                                nick = True
+                            i += 1
+
+                        print "nick: ", nick
+                        if nick == False:
+                            print ("yoo")
+                            broadcast(server_socket, sock, "\r" + '[' + str(sock.getpeername()) + '] ' + data)
+
+                    else:
+                        # remove the socket that's broken
+                        if sock in SOCKET_LIST:
+                            SOCKET_LIST.remove(sock)
+
+                        # at this stage, no data means probably the connection has been broken
+                        broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+
+                        # exception
+                except:
+                    broadcast(server_socket, sock, "Client (%s, %s) is offline\n" % addr)
+                    continue
+
+    server_socket.close()
+
+
+# broadcast chat messages to all connected clients
+def broadcast(server_socket, sock, message):
+    for socket in SOCKET_LIST:
+        # send the message only to peer
+        if socket != server_socket and socket != sock:
+            try:
+                socket.send(message)
+            except:
+                # broken socket connection
+                socket.close()
+                # broken socket, remove it
+                if socket in SOCKET_LIST:
+                    SOCKET_LIST.remove(socket)
+
+
+if __name__ == "__main__":
+    sys.exit(chat_server())
